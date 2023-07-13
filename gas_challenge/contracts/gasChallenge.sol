7b01adc21ee64688daa505c9b83a8a40f5bf2126
Lateef Olanrewaju Akinola <akinolalateefolanrewaju@gmail.com>
2023-07-13 11:18:53 +0000

    initial optimisation

--- gas_challenge/contracts/gasChallenge.sol
+++ gas_challenge/contracts/gasChallenge.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 contract gasChallenge {
     //Implement Fixed-Size Array Technique Here
-    uint[] numbers = [1,2,3,4,5,6,7,8,9,10];
+    uint[10] numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]; // Using fixed-size array
     
     //Function to check for sum of array
     //No changes required in this function
@@ -24,6 +24,11 @@ contract gasChallenge {
     //Implement Remaining Gas Optimization Techniques Here
     //Sum of elements in the numbers array should equal 0
     function optimizedFunction() public {
-      
+        uint arrayLength = numbers.length; // Caching the length of the numbers array in a local variable to avoid repeated storage reads in the loop
+        unchecked { // Unchecked block
+            for (uint i = 0; i < arrayLength; ++i) { // Different for loop increment syntax
+                    numbers[i] = 0;
+            }
+        }
     }
- }
+}
